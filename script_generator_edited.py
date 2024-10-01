d45b0bdd745d61aac9f266d8127df710fa3a8153
Tim Lüecke <tlueecke@gmail.com>
2024-10-01 23:38:06 +0200

    Added script for manual PDF generation from edited script

---
+++ script_generator_edited.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import json
+import tqdm
+import subprocess
+
+def create_asciidoc_script(json_path):
+    
+    with open(json_path, 'r', encoding="utf-8") as json_file:
+        data = json.load(json_file)
+
+    paragraphs = data['content']
+    print(f"Script for {len(paragraphs)} paragraphs loaded")
+    
+    output_file_path = json_path.replace(".json", ".adoc")
+    with open(output_file_path, 'w', encoding='utf-8') as writer:
+        writer.write(":imagesdir: slides\n\n")
+        
+        progress_bar = tqdm.tqdm(total=len(paragraphs), desc="Writing paragraphs")
+        
+        for paragraph in paragraphs:
+            tqdm.tqdm.write(f"Writing paragraph {paragraph['id']}")
+            slides = paragraph.get('slideCandidates', [])
+            if (len(slides) == 1):
+                writer.write(f"image::{os.path.basename(slides[0]['slide_file'])}[]\n\n")
+            writer.write(f"{paragraph['text']}\n\n")
+            progress_bar.update(1)
+            
+        progress_bar.close()
+        
+        print(f"Script generated at {output_file_path}")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script_generator_edited.py <directory>")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    json_path = os.path.join(directory, 'script_edited.json')
+
+    if not os.path.isfile(json_path):
+        print(f"Error: {json_path} does not exist.")
+        sys.exit(1)
+        
+    create_asciidoc_script(json_path)
+    
+    adoc_file_path = json_path.replace(".json", ".adoc")
+    pdf_file_path = json_path.replace(".json", ".pdf")
+
+    try:
+        subprocess.run(['asciidoctor-pdf', adoc_file_path, '-o', pdf_file_path], check=True)
+        print(f"PDF generated at {pdf_file_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error generating PDF: {e}")
